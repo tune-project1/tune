a397b3fe658153644baaf9efa2f855f1827dbb33
tune-project1 <junaidgohan@gmail.com>
2025-06-05 13:22:40 +1000

    minor fix

--- website/src/components/landing.vue
+++ website/src/components/landing.vue
@@ -21,7 +21,7 @@
 				</div> -->
 
         <div class="btn-container">
-          <a target="_blank" href="https:app.tune" class="btn btn-fancy btn-primary">
+          <a href="https://app.tune" class="btn btn-fancy btn-primary">
             <span> Signup </span>
 
             <svg
